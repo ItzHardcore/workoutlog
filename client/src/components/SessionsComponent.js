5d09da535d580e4e01aaba43092b1bac65c61d3e
Bruno Silva <36679895+ItzHardcore@users.noreply.github.com>
2024-04-02 15:17:37 +0100

    sessions out of dashboard

---
+++ client/src/components/SessionsComponent.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const SessionsComponent = ({ token }) => {
+    const [sessions, setSessions] = useState([]);
+
+    const fetchSessions = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/workoutSessions', {
+                method: 'GET',
+                headers: {
+                    Authorization: `${token}`,
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch sessions');
+            }
+
+            const data = await response.json();
+            setSessions(data);
+            console.log(data);
+        } catch (error) {
+            console.error('Failed to fetch sessions:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchSessions();
+    },[token]); // Call fetchSessions on component mount
+
+    const calculateVolume = exercise => {
+        let volume = 0;
+        // Iterate through each series of the exercise and calculate volume
+        exercise.series.forEach(series => {
+            volume += series.reps * series.weight;
+        });
+        return volume;
+    };
+
+    const calculateDuration = (startDate, endDate) => {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+
+        // Calculate the difference in milliseconds
+        const durationMs = end - start;
+
+        // Convert milliseconds to hours, minutes, and seconds
+        const hours = Math.floor(durationMs / (1000 * 60 * 60));
+        const minutes = Math.floor((durationMs % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((durationMs % (1000 * 60)) / 1000);
+
+        // Initialize an empty array to store non-zero duration components
+        const durationComponents = [];
+
+        // Add non-zero components to the array
+        if (hours > 0) {
+            durationComponents.push(`${hours}h`);
+        }
+        if (minutes > 0) {
+            durationComponents.push(`${minutes}min`);
+        }
+        if (seconds > 0) {
+            durationComponents.push(`${seconds}s`);
+        }
+
+        // Join the components with a space separator
+        return durationComponents.join(' ');
+    };
+
+    const calculateTotalVolume = exercises => {
+        let totalVolume = 0;
+        // Iterate through each exercise and calculate volume
+        exercises.forEach(exercise => {
+            totalVolume += calculateVolume(exercise);
+        });
+        return totalVolume;
+    };
+
+    return (
+        <div style={{ display: 'block', overflowX: 'auto' }}>
+            <h3>My Sessions</h3>
+            <div className="row row-cols-1 row-cols-sm-2 g-4 d-flex flex-nowrap">
+                {sessions.map(session => (
+                    <div key={session._id} className="col">
+                        <div className="card">
+                            <div className="card-body">
+                                <h4 className="card-title mb-0 text-capitalize">{session.workoutName}</h4>
+                                <p className="card-text">Total volume: {calculateTotalVolume(session.exercises)} Kg</p>
+                                {session.endDate && (
+                                    <p className="card-text position-absolute bottom-0 end-0 m-3">Duration: {calculateDuration(session.startDate, session.endDate)}</p>
+                                )}
+
+                                <h5 className="card-title">Exercises:</h5>
+                                <div className='mb-4'>
+                                    {session.exercises.slice(0, 3).map((exercise, index) => (
+                                        <h6 className="card-text mb-2" key={index}>{exercise.series.length} {exercise.series.length === 1 ? 'set' : 'sets'} of {exercise.name} @ Volume: {calculateVolume(exercise)} Kg</h6>
+                                    ))}
+                                </div>
+                                <Link to={`/session/${session._id}`} className="btn btn-primary">View Details</Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default SessionsComponent;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -8,6 +8,7 @@ import DatePicker from 'react-datepicker';
 import Chart from 'chart.js/auto';
 import BodyPhotosUpload from '../components/BodyPhotosUpload';
 import BodyPhotosGallery from '../components/BodyPhotosGallery';
+import SessionsComponent from '../components/SessionsComponent';
 
 function Dashboard({ token, handleLogout }) {
   const navigate = useNavigate();
@@ -15,7 +16,6 @@ function Dashboard({ token, handleLogout }) {
   const [measures, setMeasures] = useState([]); // Add state for measures
   const [Bodymeasures, setBodyMeasures] = useState([]);
   const [isWorkoutsVisible, setIsWorkoutsVisible] = useState(true);
-  const [isSessionsVisible, setIsSessionsVisible] = useState(false);
   const [isMeasuresVisible, setIsMeasuresVisible] = useState(false); // Add state for measures visibility
   const [isBodyMeasuresVisible, setIsBodyMeasuresVisible] = useState(false);
   const [isAddWorkoutsVisible, setAddIsWorkoutsVisible] = useState(true);
@@ -42,9 +42,6 @@ function Dashboard({ token, handleLogout }) {
   const toggleAddWorkout = () => {
     setAddIsWorkoutsVisible(prev => !prev);
   };
-  const toggleSessions = () => {
-    setIsSessionsVisible(prev => !prev);
-  };
 
   const toggleAddMeasures = () => {
     setAddIsMeasuresVisible((prev) => !prev);
@@ -61,7 +58,6 @@ function Dashboard({ token, handleLogout }) {
     // Fetch workouts
     fetchWorkouts(token);
     fetchMeasures(token);
-    fetchSessions(token);
   }, [token]);
 
   const WeightChart = ({ measures }) => {
@@ -107,29 +103,6 @@ function Dashboard({ token, handleLogout }) {
 
     return <canvas ref={chartRef} />;
   };
-
-
-  const fetchSessions = async (token) => {
-    try {
-      const response = await fetch('http://localhost:3001/workoutSessions', {
-        method: 'GET',
-        headers: {
-          Authorization: `${token}`,
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch sessions');
-      }
-
-      const data = await response.json();
-      setSessions(data);
-      console.log(data);
-    } catch (error) {
-      console.error('Failed to fetch sessions:', error);
-    }
-  };
-
 
   const Modal = ({ onClose, workouts, handleStartWorkoutFromModal }) => {
     const [selectedWorkoutId, setSelectedWorkoutId] = useState('');
@@ -401,56 +374,6 @@ function Dashboard({ token, handleLogout }) {
     }
   };
 
-  const calculateDuration = (startDate, endDate) => {
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-
-    // Calculate the difference in milliseconds
-    const durationMs = end - start;
-
-    // Convert milliseconds to hours, minutes, and seconds
-    const hours = Math.floor(durationMs / (1000 * 60 * 60));
-    const minutes = Math.floor((durationMs % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((durationMs % (1000 * 60)) / 1000);
-
-    // Initialize an empty array to store non-zero duration components
-    const durationComponents = [];
-
-    // Add non-zero components to the array
-    if (hours > 0) {
-      durationComponents.push(`${hours}h`);
-    }
-    if (minutes > 0) {
-      durationComponents.push(`${minutes}min`);
-    }
-    if (seconds > 0) {
-      durationComponents.push(`${seconds}s`);
-    }
-
-    // Join the components with a space separator
-    return durationComponents.join(' ');
-  };
-
-
-  const calculateVolume = (exercise) => {
-    let volume = 0;
-    // Iterate through each series of the exercise and calculate volume
-    exercise.series.forEach(series => {
-      volume += series.reps * series.weight;
-    });
-    return volume;
-  };
-
-  // Function to calculate the total volume for all exercises in a session
-  const calculateTotalVolume = (exercises) => {
-    let totalVolume = 0;
-    // Iterate through each exercise and calculate volume
-    exercises.forEach(exercise => {
-      totalVolume += calculateVolume(exercise);
-    });
-    return totalVolume;
-  };
-
   const handleEditWorkout = (workoutId) => {
     // Navigate to the edit page, you can use React Router for this
     navigate(`/edit/${workoutId}`);
@@ -586,7 +509,6 @@ function Dashboard({ token, handleLogout }) {
       <button className="btn btn-secondary ms-2 mb-2" onClick={toggleVisibility}>Toggle Workouts</button>
       <button className="btn btn-secondary ms-2 mb-2" onClick={toggleMeasures}>Toggle Measures</button>
       <button className="btn btn-secondary ms-2 mb-2" onClick={toggleBodyMeasures}>Toggle Body Measures</button>
-      <button className="btn btn-secondary ms-2 mb-2" onClick={toggleSessions}>Toggle Sessions</button>
       <button className="btn btn-success ms-2 mb-2" onClick={toggleAddWorkout}>Add Workout</button>
       <button className="btn btn-success ms-2 mb-2" onClick={toggleAddMeasures}>Add Measures</button>
       <button className="btn btn-success ms-2 mb-2" onClick={toggleAddMoreMeasures}>Body Measures</button>
@@ -595,38 +517,8 @@ function Dashboard({ token, handleLogout }) {
       {showModal &&
         <Modal onClose={() => setShowModal(false)} workouts={workouts} handleStartWorkoutFromModal={handleStartWorkout} />
       }
-      <div style={{ display: isSessionsVisible ? 'block' : 'none', overflowX: 'auto' }}>
-        <h3>My Sessions</h3>
-        <div className="row row-cols-1 row-cols-sm-2 g-4 d-flex flex-nowrap">
-          {sessions.map(session => (
-            <div key={session._id} className="col">
-              <div className="card">
-                <div className="card-body">
-                  <h4 className="card-title mb-0 text-capitalize">{session.workoutName}</h4>
-                  <p className="card-text">Total volume: {calculateTotalVolume(session.exercises)} Kg</p>
-                  {/* Calculate and display session duration */}
-                  {session.endDate && (
-                    <p className="card-text position-absolute bottom-0 end-0 m-3">Duration: {calculateDuration(session.startDate, session.endDate)}</p>
-                  )}
-
-
-                  <h5 className="card-title">Exercises:</h5>
-                  <div className='mb-4'>
-                    {session.exercises.slice(0, 3).map((exercise, index) => (
-
-                      <h6 className="card-text mb-2">{exercise.series.length} {exercise.series.length === 1 ? 'set' : 'sets'} of {exercise.name} @ Volume: {calculateVolume(exercise)} Kg</h6>
-
-                    ))}
-                  </div>
-                  {/* Link to view session details */}
-                  <Link to={`/session/${session._id}`} className="btn btn-primary">View Details</Link>
-                </div>
-              </div>
-            </div>
-          ))}
-
-        </div>
-      </div>
+     
+     <SessionsComponent token={token} />
 
 
       <div style={{ display: isAddMeasuresVisible ? 'block' : 'none' }}>
@@ -937,9 +829,9 @@ function Dashboard({ token, handleLogout }) {
             </tbody>
           </table>
         </div>
-        {saveMeasureError && (
+        {saveBodyMeasureError && (
           <div className="alert alert-danger mt-2 d-table-cell" role="alert">
-            {saveMeasureError}
+            {saveBodyMeasureError}
           </div>
         )}
 
